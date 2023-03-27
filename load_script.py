032472c8c3cf2badf40e7a49b7da508a64cb1717
Lorenzo G <lorenzo.gurgo@gmail.com>
2023-03-27 13:45:22 +0200

    script load bdd

--- load_script.py
+++ load_script.py
@@ -1,1 +1,33 @@
-import sqlite3
+import os
+import pandas as pd
+from sqlalchemy import create_engine
+
+# Créer une connexion à la base de données PostgreSQL
+import psycopg2
+conn = psycopg2.connect(
+    host="localhost",
+    database="olist_test",
+    user="postgres",
+    password="greta2023"
+)
+
+# Créer un moteur SQLAlchemy pour la connexion à la base de données PostgreSQL
+engine = create_engine('postgresql+psycopg2://', creator=lambda: conn, echo=False)
+
+
+# Chemin d'accès au dossier contenant les fichiers CSV
+path = r'C:\Users\loren\Desktop\MOOC\MOOC_G4_Maud_Lorenzo\Olist_Adile_Evelyne_Lorenzo\archive'
+
+# Parcourir tous les fichiers CSV dans le dossier et les insérer dans la base de données
+for filename in os.listdir(path):
+    if filename.endswith('.csv'):
+        # Charger le fichier CSV dans un dataframe pandas
+        df = pd.read_csv(os.path.join(path, filename))
+        # Supprimer les espaces de début et de fin des noms de colonnes
+        df.columns = df.columns.str.strip()
+        # Insérer les données dans la base de données SQLite
+        df.to_sql(filename[:-4], engine, if_exists='replace', index=False)
+
+# Fermer la connexion à la base de données
+engine.dispose()
+
---
+++ test.py
@@ -0,0 +1,21 @@
+import psycopg2
+
+# Connexion à la base de données PostgreSQL
+conn = psycopg2.connect(
+    host="localhost",
+    database="olist_test",
+    user="postgres",
+    password="greta2023"
+)
+
+# Création d'un curseur pour exécuter des requêtes SQL
+cursor = conn.cursor()
+
+# Exemple : Exécuter une requête SQL pour afficher le contenu de la table "mytable"
+cursor.execute("SELECT * FROM test")
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+
+# Fermeture de la connexion à la base de données PostgreSQL
+conn.close()
