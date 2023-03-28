c74871fe1b06b0e775cb26b28df07887e9b5c9ac
Lorenzo G <lorenzo.gurgo@gmail.com>
2023-03-28 09:28:14 +0200

    config.yml tunnel ssh

--- main.py
+++ main.py
@@ -4,6 +4,23 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import os
+import yaml
+from sshtunnel import SSHTunnelForwarder
+
+def load_config(file_path):
+    with open(file_path, "r") as file:
+        config = yaml.safe_load(file)
+    return config
+
+def create_ssh_tunnel(config):
+    ssh_tunnel = SSHTunnelForwarder(
+        (config["ssh"]["host"], 22),
+        ssh_username=config["ssh"]["username"],
+        ssh_password=config["ssh"]["password"],
+        local_bind_address=(config["ssh"]["local_bind_address"], config["ssh"]["local_bind_port"]),
+        remote_bind_address=(config["ssh"]["remote_bind_address"], config["ssh"]["remote_bind_port"]),
+    )
+    return ssh_tunnel
 
 app = FastAPI()
 
@@ -11,12 +28,16 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 # Paramètres de connexion à la base de données PostgreSQL
-DATABASE_URL = "postgresql://postgres:greta2023@localhost:5432/olist_test"
+# DATABASE_URL = "postgresql://postgres:greta2023@localhost:5432/olist_test"
 
 # Connexion à la base de données
 async def connect_to_db():
-    conn = await asyncpg.connect(DATABASE_URL)
+    config = load_config("config.yml")
+    ssh_tunnel = create_ssh_tunnel(config)
+    ssh_tunnel.start()
+    conn = await asyncpg.connect(config["postgres"]["url"])
     return conn
+
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request, conn = Depends(connect_to_db)):
